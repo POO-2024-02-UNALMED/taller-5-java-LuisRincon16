d09f68c0adc46bc9cb30cfb80e74c76d4580dd60
LuisRincon16 <lurinconj@unal.edu.co>
2024-12-10 09:21:52 +0000

    int5

---
+++ src/main/java/zooAnimales/Animal.java
@@ -0,0 +1,11 @@
+package zooAnimales;
+import java.util.ArrayList;
+
+public class Animal{
+    private int totalAnimales;
+    private String nombre;
+    private int edad;
+    private String habitat;
+    private String genero;
+    private ArrayList<Zona> zona= new ArrayList<>();
+}
